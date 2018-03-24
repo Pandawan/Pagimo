185f9f5c02a1a888c48e83357f8ed4d096a18612
PandawanFr <pandawanfr@gmail.com>
2018-03-24 14:52:01 -0700

    some firebase stuff

--- api/fire.js
+++ api/fire.js
@@ -1,3 +1,8 @@
-const firebase = require('firebase');
+const firebase = require('firebase-admin');
 
-const app = firebase.initializeApp({ PRIVATE KEYS HERE });
+const serviceAccount = require('');
+
+const app = firebase.initializeApp({
+	credential: firebase.credential.cert(serviceAccount),
+	databaseURL: 'https://pagimo-hack.firebaseio.com'
+});
