b9c73bbc6bef0c80e34c4c432a6bebcb4911aacf
PandawanFr <pandawanfr@gmail.com>
2018-03-24 16:07:34 -0700

    FireDB and stuff

--- api/fire.js
+++ api/fire.js
@@ -1,16 +1,42 @@
 const admin = require('firebase-admin');
 const serviceAccount = require('../creds.json');
+const slug = require('slug');
 
-const db = admin.firestore();
 const app = admin.initializeApp({
 	credential: admin.credential.cert(serviceAccount),
 	databaseURL: 'https://pagimo-hack.firebaseio.com'
 });
 
-const docRef = db.collection('posts').doc('test');
-const set = docRef.set({
-	content: 'hello',
-	created: Date.now(),
-	owner: 'ya boi',
-	title: 'Testing testing 123'
+const db = admin.firestore();
+
+const authRef = db.collection('auth');
+const usersRef = db.collection('users');
+const postsRef = db.collection('posts');
+
+/**
+ * Promise with user data (see users database to see user structure)
+ * @param {string} id The user's id to search for
+ */
+module.exports.getUser = id => new Promise((resolve, reject) => {
+	const user = usersRef.doc(id);
+	user.get().then((doc) => {
+		if (doc.exists) {
+			resolve(doc.data());
+		}
+		else {
+			resolve(null);
+		}
+	}).catch((error) => {
+		reject(error);
+	});
+});
+
+
+/**
+ * Create a user in the Firebase DB
+ * @param {string} uid User UID (from Google Sign In or other)
+ * @param {string} name User's Name (from Google Sign In or other)
+ */
+module.exports.createUser = (uid, name) => new Promise((resolve, reject) => {
+	const userId = slug(name);
 });
--- app.js
+++ app.js
@@ -6,7 +6,8 @@ const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
 
 const index = require('./routes/index');
-const users = require('./routes/users');
+const user = require('./routes/user');
+const investors = require('./routes/investors');
 
 const app = express();
 
@@ -25,7 +26,8 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
-app.use('/users', users);
+app.use('/user', user);
+app.use('/investors', investors);
 
 // catch 404 and forward to error handler
 app.use((req, res, next) => {
