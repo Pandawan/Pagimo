ad73b004cb60840630be466191f52b038913f410
Rohan Pandey <rohan.pandey@gmail.com>
2018-03-24 20:55:50 -0700

    sketch

--- sketch.py
+++ sketch.py
@@ -3,26 +3,34 @@ class Investor(): # client side functions
 	tokens = 0
 	portfolio = dict()
 
-	def addMoney(usd):
+	def deposit(str name, int usd): # to add tokens
+		this.name = name
 		tokens += usd
 
+	def withdraw(int tokens): # to convert out to USD
+		this.tokens -= tokens
+		return tokens
+
 	def sell(Channel channel, int minPrice, int num):
-		Market.postSell(this.name, channel, minPrice, num)
+		Market.postSell(this, channel, minPrice, num)
 
-	def buy(Channel channel, int num):
-		Market.postBuy(this.name, channel, askPrice, num)
+	def buy(Channel channel,int askPrice, int num):
+		Market.postBuy(this, channel, askPrice, num)
 
-	def takeMoney(tokenAmt):
-		tokens -= tokenAmt
-		return tokenAmt
+class Channel(Investor): # inherits from Investor
+	marketCap = portfolio[name] * shareVal()
 
-class Channel():
-	marketCap = 0
-	shares = 0
-	shareValue = marketCap/sharesTotal
+	# decide how many shares to split channel into
+	def startChannel(str name, int shares):
+		this.name = name
+		portfolio[name] = shares
 
-	def startChannel(int shareAmt):
-		shares = shareAmt
+	# can only sell and buy shares of own channel
+	def sellShares(int minPrice, int num):
+		Market.postSell(super, this, minPrice, num)
+
+	def buyShares(int askPrice, int num):
+		Market.postBuy(super, this, askPrice, num)
 
 class Market(): # this is the server
 	sell = {} # {channel:[[seller, askPrice, num],[seller2, askPrice2, num2]]}
@@ -30,22 +38,29 @@ class Market(): # this is the server
 
 	def postSell(Investor seller, Channel channel, int minPrice, int num):
 		for i in buy[channel]:
-			if minPrice < buy[channel][i][1]:
-				sell[channel][i][0].portfolio[channel] -= sell[channel][i][2]
-				seller.portfolio[channel] -= 
-				# remove the number of shares to be sold from the sellers portfolio
-				buyer.portfolio[channel] -= sell[channel][i][2]
-				# add the number of shares to be bought to the buyers portfolio
+			buyEntry = buy[channel][i]
+			if minPrice < buyEntry[1]: # if minPrice < askPrice
+				seller.portfolio[channel] -= buyEntry[2]
+				buyEntry[0].portfolio[channel] += buyEntry[2]
+				# transfer buyer's num amount of shares of channel from seller to buyer
+				buyEntry[0].tokens -= buyEntry[1]
+				seller.tokens += buyEntry[1]
+				# transfer buyer's askPrice amount of tokens from buyer to seller
 				return "Succesful sale"
 		sell[channel].append([seller, minPrice, num])
+		return "Posted to market, waiting for purchase"
 
 	def postBuy(Investor buyer, Channel channel, int askPrice, int num):
-		for i in sell[channel]:
-			if askPrice > sell[channel][i][1]:
-				sell[channel][i][0].portfolio[channel] -= sell[channel][i][2]
-				# remove the number of shares to be sold from the sellers portfolio
-				buyer.portfolio[channel] += sell[channel][i][2]
-				# add the number of shares to be bought to the buyers portfolio
+		for i in sell[channel]: # shares to be sold
+			sellEntry = sell[channel][i]
+			if askPrice > sellEntry[1]: # if askPrice > minPrice
+				sellEntry[0].portfolio[channel] -= sellEntry[2]
+				buyer.portfolio[channel] += sellEntry[2]
+				# transfer num amount of shares of channel from seller to buyer
+				buyer.tokens -= askPrice
+				sellEntry[0].tokens += askPrice
+				# transfer askPrice amount of tokens from buyer to seller
 				return "Succesful purchase"
 		buy[channel].append([buyer, askPrice, num])
 		# if no sell that meets criteria is found, post to buy dict
+		return "Posted to market, waiting for sale"
